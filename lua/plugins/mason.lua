9d484abdcae51b810e437f444275d5b5406bb5fb
matt jennings <mattjennings@matts-MacBook-Air.local>
2025-06-09 20:42:17 -0400

    uhh

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,7 +1,6 @@
 return {
     "williamboman/mason.nvim",
     dependencies = {
-        "williamboman/mason-lspconfig.nvim",
         "WhoIsSethDaniel/mason-tool-installer.nvim",
     },
     config = function()
@@ -9,8 +8,6 @@ return {
         local mason = require("mason")
 
         -- import mason-lspconfig
-        local mason_lspconfig = require("mason-lspconfig")
-
         local mason_tool_installer = require("mason-tool-installer")
 
         -- enable mason and configure icons
@@ -23,9 +20,6 @@ return {
                 },
             },
         })
-        mason_lspconfig.setup {
-            ensure_installed = { "clangd", "pyright", "ts_ls", "lua_ls", "html", "cssls", "gopls" } -- Add your language servers here
-        }
 
 
 
